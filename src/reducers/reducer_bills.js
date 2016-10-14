0b876c636dabf925d9dbc01b92801b9fd61be324
Richi Jeffery <kikki_sawn@yahoo.com>
2016-10-13 18:12:02 -0700

    remove console.logs

--- src/reducers/reducer_bills.js
+++ src/reducers/reducer_bills.js
@@ -3,7 +3,7 @@ import { FETCH_BILLS } from '../actions/index';
 export default function(state = [], action) {
   switch (action.type) {
   case FETCH_BILLS:
-  console.log(action.payload.data);
+  // console.log(action.payload.data);
     return [ action.payload.data, ...state ];
   }
 
